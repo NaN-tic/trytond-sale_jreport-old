5f93040c4c6c9560a7d3f9fd1e273bff7f9816c9
resteve <resteve@zikzakmedia.com>
2013-04-29 09:19:22 +0200

    pyflakes fixed

--- sale.py
+++ sale.py
@@ -2,7 +2,6 @@
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from trytond.pool import PoolMeta
-from trytond.transaction import Transaction
 from trytond.modules.jasper_reports.jasper import JasperReport
 
 __all__ = ['SaleReport']
